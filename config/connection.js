df62159ea88a664a0dd96ac9712359786f216c2f
Priyam Carto <cpriyam90@gmail.com>
2022-06-06 19:13:09 -0400

    initial codes merged

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+require('dotenv').config(); 
+
+
+
+//logic for heroku and local db 
+if(process.env.JAWSDB_URL){
+    sequelize = new sequelize(proces.env.JAWSDB_URL); 
+} else { 
+    sequelize = new Sequelize (
+        process.env.DB_NAME, 
+        process.env.DB_USER, 
+        process.env.DB_PASSWORD, 
+        {
+            host: 'localhost',
+            dialect: 'mysql', 
+            port: 3306,
+        }
+    );
+}
+
+module.exports = sequelize; 
