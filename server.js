d96eb0109304f4c556723358437f28b50244e679
Priyam Carto <cpriyam90@gmail.com>
2022-06-10 21:26:11 -0400

    server.js update

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const session = require('express-session');
 
 //// 
 
+const express = require('express'); 
+const session = require('express-session'); 
+//handlebars const should go here 
+
+//// 
+
 const sequelizeStore = require('connect-session-sequelize')(session.Store); 
 const path = require('path'); 
 const sequelize = require('./config/connection');
